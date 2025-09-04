16afd33079e9c26480cab612d0a835441d9567a7
Michael Pogodin <miker2808@gmail.com>
2025-09-04 15:44:05 +0300

    UI runs!

---
+++ ui/src/MainController.java
@@ -0,0 +1,19 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class MainController {
+
+    @FXML
+    private Label myLabel;
+
+    @FXML
+    private void onButtonClick() {
+        // Called when the button is clicked
+        myLabel.setText("Button was clicked!");
+    }
+
+    // Public method so UserInterface or other classes can update UI
+    public void setLabelText(String text) {
+        myLabel.setText(text);
+    }
+}
--- ui/src/UserInterface.java
+++ ui/src/UserInterface.java
@@ -1,25 +1,55 @@
-package ui;
-
 import engine.Engine;
 import engine.ExecutionHistory;
 import engine.SProgram;
 import engine.execution.ExecutionResult;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
-public class UserInterface {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+
+public class UserInterface extends Application{
     Engine engine;
     Scanner scanner;
 
+    @Override
+    public void start(Stage primaryStage) throws Exception {
 
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/main.fxml"));
+
+        Parent root = loader.load();
+
+        // Get the controller (defined in FXML)
+        MainController controller = loader.getController();
+
+        // Example: update the UI once it's loaded
+        controller.setLabelText("Hello from UserInterface!");
+
+        // Setup the stage
+        primaryStage.setTitle("My App");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+
+    /*
     public static void main(String[] args) {
         UserInterface ui = new UserInterface();
         ui.run();
     }
-
+    */
 
     public void run() {
         engine = new Engine();
